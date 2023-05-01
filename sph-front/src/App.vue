a562e062deafba99055f99a3bde92d69b9f925f5
18866805256 <3407085928@qq.com>
2023-05-01 17:46:04 +0800

    day01

---
+++ sph-front/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <Header></Header>
+    <!-- 路由组件出口文件 -->
+    <router-view></router-view>
+    <!-- 在Home、search显示，在登录、注册隐藏 -->
+    <Footer v-show="$route.meta.show"></Footer>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+export default {
+  name: 'App',
+  components: {
+    Header, Footer
+  }
+}
+</script>
+
+<style></style>
---
+++ sph-front/src/pages/Home/index.vue
@@ -0,0 +1,13 @@
+<template lang="">
+  <div>
+    我是主页
+  </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style lang="">
+  
+</style>
