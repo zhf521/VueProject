5db814cf7819503f98fee6f0c02a1c8549aea99d
18866805256 <3407085928@qq.com>
2023-05-01 18:27:04 +0800

    重写push和replace方法

--- sph-front/src/router/index.js
+++ sph-front/src/router/index.js
@@ -9,6 +9,41 @@ import Home from '../pages/Home'
 import Search from '../pages/Search'
 import Login from '../pages/Login'
 import Register from '../pages/Register'
+//先把VueRouter原型对象的push和replace保存一份
+let originPush = VueRouter.prototype.push
+let originReplace = VueRouter.prototype.replace
+//重写push|replace方法
+//第一个参数：告诉原来push方法，你往哪里跳转（传递哪些参数）
+//第二个参数：成功的回调
+//第三个参数：失败的回调
+//call||apply区别
+//相同点：都可以调用函数一次，都可以篡改函数的上下文一次
+//不同点：call与apply传递参数，call传递参数用逗号隔开，apply方法执行，传递数组
+VueRouter.prototype.push = function (location, resolve, reject) {
+  if (resolve && reject) {
+    originPush.call(this, location, resolve, reject)
+  } else {
+    originPush.call(
+      this,
+      location,
+      () => {},
+      () => {}
+    )
+  }
+}
+VueRouter.prototype.replace = function (location, resolve, reject) {
+  if (resolve && reject) {
+    originReplace.call(this, location, resolve, reject)
+  } else {
+    originReplace.call(
+      this,
+      location,
+      () => {},
+      () => {}
+    )
+  }
+}
+
 //配置路由
 export default new VueRouter({
   //配置路由
@@ -22,7 +57,7 @@ export default new VueRouter({
       path: '/search/:keyword',
       component: Search,
       meta: { show: true },
-      name:'search',
+      name: 'search',
     },
     {
       path: '/login',
