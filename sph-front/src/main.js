767a038feef1f7e3971f8de63e2574fe0f3ed1bc
18866805256 <3407085928@qq.com>
2023-05-02 21:41:19 +0800

    update

--- sph-front/src/main.js
+++ sph-front/src/main.js
@@ -6,6 +6,8 @@ import TypeNav from './pages/Home/TypeNav'
 Vue.component(TypeNav.name, TypeNav)
 //引入路由
 import router from './router'
+//引入仓库
+import store from './store'
 //测试
 import { reqCategoryList } from './api'
 reqCategoryList()
@@ -14,4 +16,6 @@ new Vue({
   render: (h) => h(App),
   //注册路由
   router,
+  //注册仓库：组件实例的身上会多一个$store属性
+  store,
 }).$mount('#app')
