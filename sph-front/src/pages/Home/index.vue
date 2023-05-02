1ae46f61d048ddd39658897c3a9e8c884b69078e
18866805256 <3407085928@qq.com>
2023-05-02 17:50:40 +0800

    update

--- sph-front/src/pages/Home/index.vue
+++ sph-front/src/pages/Home/index.vue
@@ -1,11 +1,26 @@
 <template lang="">
   <div>
-    我是主页
+    <!-- 三级联动全局组件：三级联动已经注册为全局组件，因此不需要再引入 -->
+    <TypeNav></TypeNav>
+    <ListContainer></ListContainer>
+    <Recommend></Recommend>
+    <Rank></Rank>
+    <Like></Like>
+    <Floor></Floor>
+    <Floor></Floor>
+    <Brand></Brand>
   </div>
 </template>
 <script>
+//引入其余的组件
+import ListContainer from '../Home/ListContainer'
+import Recommend from '../Home/Recommend'
+import Rank from '../Home/Rank'
+import Like from '../Home/Like'
+import Floor from '../Home/Floor'
+import Brand from '../Home/Brand'
 export default {
-
+  components: { ListContainer, Recommend, Rank, Like, Floor, Brand },
 }
 </script>
 <style lang="">
