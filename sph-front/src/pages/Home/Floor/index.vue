41afbb1c9ab1bda6e21f2b2d66a6c8e207117d0d
18866805256 <3407085928@qq.com>
2023-05-02 18:18:48 +0800

    update

--- sph-front/src/pages/Home/Floor/index.vue
+++ sph-front/src/pages/Home/Floor/index.vue
@@ -50,12 +50,12 @@
                   <div class="swiper-slide">
                     <img src="./images/floor-1-b01.png">
                   </div>
-                  <div class="swiper-slide">
+                  <!-- <div class="swiper-slide">
                     <img src="./images/floor-1-b02.png">
-                  </div>
-                  <div class="swiper-slide">
+                  </div> -->
+                  <!-- <div class="swiper-slide">
                     <img src="./images/floor-1-b03.png">
-                  </div>
+                  </div> -->
                 </div>
                 <!-- 如果需要分页器 -->
                 <div class="swiper-pagination"></div>
--- sph-front/src/pages/Home/ListContainer/index.vue
+++ sph-front/src/pages/Home/ListContainer/index.vue
@@ -9,15 +9,15 @@
             <div class="swiper-slide">
               <img src="./images/banner1.jpg" />
             </div>
-            <div class="swiper-slide">
+            <!-- <div class="swiper-slide">
               <img src="./images/banner2.jpg" />
-            </div>
-            <div class="swiper-slide">
+            </div> -->
+            <!-- <div class="swiper-slide">
               <img src="./images/banner3.jpg" />
-            </div>
-            <div class="swiper-slide">
+            </div> -->
+            <!-- <div class="swiper-slide">
               <img src="./images/banner4.jpg" />
-            </div>
+            </div> -->
           </div>
           <!-- 如果需要分页器 -->
           <div class="swiper-pagination"></div>
