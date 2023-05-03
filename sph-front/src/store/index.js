865dccf324a3b8ae548343869be9766ad3e9d28e
18866805256 <3407085928@qq.com>
2023-05-03 14:23:50 +0800

    update

--- sph-front/src/store/index.js
+++ sph-front/src/store/index.js
@@ -2,15 +2,14 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 //需要使用插件
 Vue.use(Vuex)
-//state:仓库存储数据的地方
-const state = {}
-//mutations:修改state的唯一手段
-const mutations = {}
-//actions:处理action，可以书写自己的业务逻辑，也可以处理异步
-const actions = {}
-//getters:理解为计算属性，用于简化仓库数据，让组件获取仓库的数据更加方便
-const getters = {}
+//引入小仓库
+import home from './home'
+import search from './search'
 //对外暴露Store类的一个实例
 export default new Vuex.Store({
-state,mutations,actions,getters
+  //实现Vuex仓库模块式开发存储数据
+  modules: {
+    home,
+    search,
+  },
 })
---
+++ sph-front/src/store/search/index.js
@@ -0,0 +1,17 @@
+//search模块的小仓库
+//state:仓库存储数据的地方
+const state = {}
+//mutations:修改state的唯一手段
+const mutations = {}
+//actions:处理action，可以书写自己的业务逻辑，也可以处理异步
+const actions = {
+  //这里可以书写业务逻辑，但是不能修改state
+}
+//getters:理解为计算属性，用于简化仓库数据，让组件获取仓库的数据更加方便
+const getters = {}
+export default {
+  state,
+  mutations,
+  actions,
+  getters,
+}
