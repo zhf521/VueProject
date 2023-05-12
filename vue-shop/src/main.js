26304328a32bd1ea22e0f7b0a6de36cd97caedd9
18866805256 <3407085928@qq.com>
2023-05-12 15:57:15 +0800

    完成登录功能

--- vue-shop/src/main.js
+++ vue-shop/src/main.js
@@ -1,8 +1,28 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+// 引入ElementUI组件库
+import ElementUI from 'element-ui'
+// 引入ElementUI全部样式
+import 'element-ui/lib/theme-chalk/index.css'
+//引入VueRouter
+import VueRouter from 'vue-router'
+//引入路由器
+import router from './router'
+//导入全局样式表
+import './assets/CSS/global.css'
+//引入axios
+import axios from 'axios'
+//配置请求的根路径
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
+//应用路由插件
+Vue.use(VueRouter)
+//应用ElementUI
+Vue.use(ElementUI)
+
 new Vue({
-  render: h => h(App),
+  render: (h) => h(App),
+  router,
 }).$mount('#app')
---
+++ vue-shop/src/router/index.js
@@ -0,0 +1,36 @@
+//引入VueRouter
+import VueRouter from 'vue-router'
+//引入路由组件
+import Login from '../components/Login.vue'
+import Home from '../components/Home.vue'
+
+//创建并暴露router实例对象，去管理路由规则
+
+const router = new VueRouter({
+  routes: [
+    { path: '/', redirect: '/login' },
+    { path: '/login', component: Login },
+    { path: '/home', component: Home },
+  ],
+})
+
+//挂载路由导航守卫
+router.beforeEach((to, from, next) => {
+  //to 将要访问的路径
+  //from 代表从哪个路径跳转而来
+  //next 是一个函数，表示放行
+  //next() 放行
+  //next('/login') 强制跳转
+
+  //如果用户访问的登录页，直接放行
+  if (to.path === '/login') return next()
+  //获取token
+  const tokenStr = window.sessionStorage.getItem('token')
+  //没有token，强制跳转到登录页
+  if (!tokenStr) {
+    return next('/login')
+  }
+  next()
+})
+
+export default router
