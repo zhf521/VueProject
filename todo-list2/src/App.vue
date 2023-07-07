a619f411acc29120ca85477ee83c98dba2e1329a
18866805256 <3407085928@qq.com>
2023-07-07 22:09:40 +0800

    样式

---
+++ todo-list2/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="app">
+    <TodoHeader />
+  </div>
+</template>
+
+<script>
+import TodoHeader from './components/TodoHeader'
+export default {
+  name: 'App',
+  components: {
+    TodoHeader
+  }
+}
+</script>
+
+<style>
+.app {
+  width: 300px;
+  height: 300px;
+  margin: 0 auto;
+  padding: 10px;
+  border: 1px solid burlywood;
+  border-radius: 5%;
+}
+</style>
---
+++ todo-list2/src/components/TodoHeader.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h2 class="title">ToDo</h2>
+    <el-input placeholder="请输入你的任务名称，按回车键确认">
+    </el-input>
+  </div>
+</template>
+<script>
+export default {
+  name: 'TodoHeader'
+}
+</script>
+<style scoped>
+.title {
+  text-align: center;
+}
+</style>
